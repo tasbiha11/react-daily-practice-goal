f34840abfd9c20cf6e4c8a236cd224fbb3c38a78
tasbiha11 <tasbihahussain@yahoo.com>
2022-09-14 00:51:48 +0600

    toogle-completed

--- src/App.js
+++ src/App.js
@@ -4,21 +4,62 @@ import { AiOutlinePlus, AiOutlineClose } from 'react-icons/ai'
 
 function App() {
   const [tasks, setTasks] = useState([])
+  const [input, setInput] = useState('')
+
+  //add task
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const addTask = {
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+      completed: false
+    }
+    setTasks([...tasks, addTask])
+    setInput('')
+  }
+
+  //delete task
+  const deleteTask = (id) => {
+    let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id)
+    setTasks(filteredTasks)
+    console.log('task deleted')
+  }
+
+  // toggle completed task
+  const toggleComplete = (id) => {
+    setTasks(
+      tasks.map(task => (
+        task.id === id ? { ...task, completed: !task.completed } : task
+      ))
+    )
+  }
 
   return (
-    <div className="app">
+    <div className='app'>
       <div className="container">
-        <h1><BiTaskX />Today's Practices</h1>
-        <form >
+        <h1>< BiTaskX /> Powerlist</h1>
+
+
+        <form onSubmit={handleSubmit}>
           <div className="form-input">
             <AiOutlinePlus className='icon' />
-            <input placeholder='Enter todays practice goals'
+            <input
+              value={input}
+              onChange={e => setInput(e.target.value)}
+              placeholder='Enter a task'
               type="text" />
           </div>
         </form>
+        <div>
+          {tasks.map(task => (
+            <div className={`task-row ${task.completed ? 'completed' : ''}`} key={task.id} onDoubleClick={() => toggleComplete(task.id)} >
+              <p>{task.text} </p>
+              <AiOutlineClose onClick={() => deleteTask(task.id)} className='icon' />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
-
 export default App;
